10b926d1540f043ef77c9aec0d11eefc06fb9cd6
BinRiver <92713328+BinRiver@users.noreply.github.com>
2022-09-19 14:07:26 +0900

    Update 평균은 넘겠지.py

--- 백준/Bronze/4344. 평균은 넘겠지/평균은 넘겠지.py
+++ 백준/Bronze/4344. 평균은 넘겠지/평균은 넘겠지.py
@@ -1,17 +1,13 @@
-test_num = int(input())
-result_list = []
+C = int(input())
 
-for i in range(0, test_num):
-    num_list = list(map(int, input().split()))
-    num = num_list[0]
-    result = 0
-    for l in range(1, num+1):
-        result += num_list[l]
-    avg = result / num
-    k = 0
-    for j in range(1, num+1):
-        if num_list[j] > avg:
-            k += 1
-    result_list.append(k/num*100)
-for i in range(0, len(result_list)):
-    print('{0:0.3f}%'.format(result_list[i]))
+for i in range(C):
+    N = list(map(int, input().split()))
+    avg = 0
+    for j in range(1, len(N)):
+        avg += N[j]
+    avg = avg/N[0]
+    count = 0
+    for num in range(1, len(N)):
+        if N[num] > avg:
+            count += 1
+    print("{:.3f}%".format(count/N[0]*100))
