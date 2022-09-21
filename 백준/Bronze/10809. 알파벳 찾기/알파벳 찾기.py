bb408d5e8df6863c26561ddd4df1691cf40d3e64
BinRiver <92713328+BinRiver@users.noreply.github.com>
2022-09-21 13:44:32 +0900

    Update 알파벳 찾기.py

--- 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.py
+++ 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.py
@@ -1,13 +1,7 @@
-s = input()
-for i in range(97, 123):
-    if chr(i) in s:
-        j = 0
-        while True:
-            if s[j] == chr(i):
-                break
-            else:
-                j += 1
-                continue
-        print(j, end=' ')
+S = list(input())
+
+for i in range(ord('a'), ord('z')+1):
+    if chr(i) in S:
+        print(S.index(chr(i)), end = " ")
     else:
-        print(-1, end=' ')
+        print("-1", end = " ")
