07d5f07bf96d70fc1f663229b72cd03b9eda84ef
BinRiver <92713328+BinRiver@users.noreply.github.com>
2022-09-23 11:13:54 +0900

    Update 크로아티아 알파벳.py

--- 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.py
+++ 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.py
@@ -1,20 +1,8 @@
-word = input()
+S = input()
 
-count = len(word)
-for i in range(len(word)):
-    if word[i] == '=':
-        if word[i-1] == 'c' or word[i-1] == 's':
-            count -= 1
-        else:
-            if word[i-2] == 'd':
-                count -= 2
-            else:
-                count -= 1
-    elif word[i] == '-':
-        count -= 1
-    elif word[i] == 'j':
-        if word[i-1] == 'l' or word[i-1] == 'n':
-            count -= 1
-    else:
-        continue
-print(count)
+c = ["c=", "c-", "dz=", "d-", "lj", "nj", "s=", "z="]
+
+for i in c:
+    if i in S:
+        S = S.replace(i, "*")
+print(len(S))
